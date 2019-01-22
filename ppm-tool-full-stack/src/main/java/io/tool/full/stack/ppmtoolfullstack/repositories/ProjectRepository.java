5c538eff686ba5bcd67dbc42c0d26d18d8701b34
Badrikant Soni <badrikant.soni@inmobi.com>
2019-01-22 10:27:25 +0530

    Update ProjectRepository.java

--- ppm-tool-full-stack/src/main/java/io/tool/full/stack/ppmtoolfullstack/repositories/ProjectRepository.java
+++ ppm-tool-full-stack/src/main/java/io/tool/full/stack/ppmtoolfullstack/repositories/ProjectRepository.java
@@ -6,6 +6,10 @@ import org.springframework.stereotype.Repository;
 
 /**
  * @author badrikant.soni on 22/01/19
+ * Q. What is "@Respository" annotation and what it does ?
+ * it is a Spring annotation that indicates that the decorated class is a repository.
+ * A repository is a mechanism for encapsulating storage, retrieval, and search behavior which emulates a collection of objects.
+ * It is a specialization of the @Component annotation allowing for implementation classes to be autodetected through classpath scanning
  */
 @Repository
 public interface ProjectRepository extends CrudRepository<Project,Long> {
