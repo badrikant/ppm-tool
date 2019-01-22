157257822a81842cef8f61745edd50ad70c9a1f0
Badrikant Soni <badrikant.soni@inmobi.com>
2019-01-22 12:02:49 +0530

    add object validations & better error response

--- ppm-tool-full-stack/src/main/java/io/tool/full/stack/ppmtoolfullstack/domain/Project.java
+++ ppm-tool-full-stack/src/main/java/io/tool/full/stack/ppmtoolfullstack/domain/Project.java
@@ -1,15 +1,19 @@
 package io.tool.full.stack.ppmtoolfullstack.domain;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.PrePersist;
 import javax.persistence.PreUpdate;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 import java.util.Date;
 
 /**
@@ -24,13 +28,29 @@ public class Project {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @NotBlank(message = "Project name is required")
     private String projectName;
+
+    @NotBlank(message = "Project identifier is required")
+    @Size(min = 4, max = 5, message = "Please use 4 to 5 characters")
+    @Column(updatable = false, unique = true)
+    // it should be unique and shouldn't be able to edit/update once it gets created.
     private String projectIdentifier;
+
+    @NotBlank(message = "Description is required")
     private String description;
+
+    @JsonFormat(pattern = "yyyy-mm-dd") // need to have better date format instead "2019-01-22T04:30:17.134+0000"
     private Date startDate;
+
+    @JsonFormat(pattern = "yyyy-mm-dd") // need to have better date format instead "2019-01-22T04:30:17.134+0000"
     private Date endDate;
 
+    @JsonFormat(pattern = "yyyy-mm-dd") // need to have better date format instead "2019-01-22T04:30:17.134+0000"
     private Date created_At;
+
+    @JsonFormat(pattern = "yyyy-mm-dd") // need to have better date format instead "2019-01-22T04:30:17.134+0000"
     private Date updated_At;
 
     @PrePersist
--- ppm-tool-full-stack/src/main/java/io/tool/full/stack/ppmtoolfullstack/web/ProjectController.java
+++ ppm-tool-full-stack/src/main/java/io/tool/full/stack/ppmtoolfullstack/web/ProjectController.java
@@ -5,10 +5,16 @@ import io.tool.full.stack.ppmtoolfullstack.services.ProjectService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 /**
  * @author badrikant.soni on 22/01/19
@@ -21,7 +27,19 @@ public class ProjectController {
     private ProjectService projectService;
 
     @PostMapping("")
-    public ResponseEntity<Project> createNewProject(@RequestBody Project project) {
+    public ResponseEntity<?> createNewProject(@Valid @RequestBody Project project, BindingResult result) {
+        // @Valid annotation - Will allow the valid request body to pass on and gives 400 instead of 500 with better error response.
+
+        // BindingResult - BindingResult is an interface that basically invokes the validator on an object.This is basically analyzes the object
+        // and it determines whether or not there are errors and if there has errors we can return invalid objects error message in response
+        if (result.hasErrors()) {
+
+            Map<String, String> errorMap = result.getFieldErrors().stream()
+                    .collect(Collectors.toMap(FieldError::getField, FieldError::getDefaultMessage));  // getting map from the list
+
+            return new ResponseEntity<Map<String, String>>(errorMap, HttpStatus.BAD_REQUEST);
+        }
+
         Project newProject = projectService.saveOrUpdate(project);
         return new ResponseEntity<Project>(newProject, HttpStatus.CREATED);
     }
