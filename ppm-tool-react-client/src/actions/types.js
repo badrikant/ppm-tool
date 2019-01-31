11654af1e76d5072f6321e4a827e0e519d64e2c6
Badrikant Soni <badrikant.soni@inmobi.com>
2019-01-31 11:23:24 +0530

    get-projects-in-redux

--- ppm-tool-react-client/src/actions/types.js
+++ ppm-tool-react-client/src/actions/types.js
@@ -1,2 +1,3 @@
 //define type of actions which needs to be performed
 export const GET_ERRORS = "GET_ERRORS";
+export const GET_PROJECTS = "GET_PROJECTS";
--- ppm-tool-react-client/src/reducers/index.js
+++ ppm-tool-react-client/src/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from "redux";
 import errorReducer from "./errorReducer";
+import projectReduer from "./projectReducer";
 
 export default combineReducers({
-  errors: errorReducer
+  errors: errorReducer,
+  project: projectReduer
 });
