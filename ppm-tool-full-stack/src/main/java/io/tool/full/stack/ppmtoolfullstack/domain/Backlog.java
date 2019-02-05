4627549c3a661689aa6dad65744cab673cb49d61
Badrikant Soni <badrikant.soni@inmobi.com>
2019-02-05 09:45:36 +0530

    establish the entities relationships

--- ppm-tool-full-stack/src/main/java/io/tool/full/stack/ppmtoolfullstack/domain/Backlog.java
+++ ppm-tool-full-stack/src/main/java/io/tool/full/stack/ppmtoolfullstack/domain/Backlog.java
@@ -1,18 +1,21 @@
 package io.tool.full.stack.ppmtoolfullstack.domain;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author badrikant.soni on 02/02/19
@@ -31,10 +34,16 @@ public class Backlog {
 
     //OneToOne with project
     @OneToOne(fetch = FetchType.EAGER) // fetch the table with any explicit request by App
-    @JoinColumn(name = "project_id",nullable = false) // create a column called "project_id" in Backlog table in DB. Also it can't be null.
+    @JoinColumn(name = "project_id", nullable = false)
+    // create a column called "project_id" in Backlog table in DB. Also it can't be null.
     @JsonIgnore // prevent the infinite recursion call when relationship established with project
     private Project project;
 
-    //OneToMany with projecttasks
+    // Use Case : A backlog can have many projectTasks but not vice versa
+    // FetchType.EAGER - fetch the ProjectTask table without explicit request by app.
+    // CascadeType.ALL - on creating/upating/deleting the backlog, project tasks will also be created/deleted/updated.
+    // OneToMany with projecttasks
+    @OneToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL, mappedBy = "backlog")
+    private List<ProjectTask> projectTasks = new ArrayList<>();
 
 }
--- ppm-tool-full-stack/src/main/java/io/tool/full/stack/ppmtoolfullstack/domain/ProjectTask.java
+++ ppm-tool-full-stack/src/main/java/io/tool/full/stack/ppmtoolfullstack/domain/ProjectTask.java
@@ -1,15 +1,20 @@
 package io.tool.full.stack.ppmtoolfullstack.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.validation.constraints.NotBlank;
 import java.util.Date;
 
@@ -37,6 +42,13 @@ public class ProjectTask {
     private Date dueDate;
 
     //ManyToOne with Backlog
+    // Use Case: There can be multiple projectTasks and those will belong to one and only one backlog.
+    // CascadeType.REFRESH - it tells that on deleting the projectTasks, backlog will be refreshed automatically and says deleted projectTask is no longer exists.
+    @ManyToOne(fetch = FetchType.EAGER, cascade = CascadeType.REFRESH)
+    @JoinColumn(name = "backlog_id", updatable = false, nullable = false)
+    // "backlog_id" will not be updatable and nullable
+    @JsonIgnore // in case of infinite recursion issue
+    private Backlog backlog;
 
     @Column(updatable = false)
     private String projectIdentifier;
