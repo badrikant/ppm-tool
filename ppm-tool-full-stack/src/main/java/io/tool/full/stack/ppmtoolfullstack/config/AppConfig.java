411c8ed356f9f54065db4999b4de205a5b9dcb11
Badrikant Soni <badrikant.soni@inmobi.com>
2019-02-18 15:11:50 +0530

    add logback.xml
    add logger functionality
    add logbook for captauring the request and response
    enable hibernate sql log

--- ppm-tool-full-stack/src/main/java/io/tool/full/stack/ppmtoolfullstack/config/AppConfig.java
+++ ppm-tool-full-stack/src/main/java/io/tool/full/stack/ppmtoolfullstack/config/AppConfig.java
@@ -3,6 +3,7 @@ package io.tool.full.stack.ppmtoolfullstack.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.filter.CommonsRequestLoggingFilter;
 
 /**
  * @author badrikant.soni on 16/02/19
@@ -13,5 +14,14 @@ public class AppConfig {
     @Bean
     public BCryptPasswordEncoder bCryptPasswordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public CommonsRequestLoggingFilter requestLoggingFilter() {
+        CommonsRequestLoggingFilter loggingFilter = new CommonsRequestLoggingFilter();
+        loggingFilter.setIncludeClientInfo(true);
+        loggingFilter.setIncludeQueryString(true);
+        loggingFilter.setIncludePayload(true);
+        return loggingFilter;
     }
 }
